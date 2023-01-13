457f84f9e9a198c4d8c3f0064100eb134f476ec9
Fozilbek <fl.raimberdiyev@mail.ru>
2023-01-13 21:39:30 +0500

    corrected header menu style

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,45 +1,47 @@
 <template>
-<div class="d-flex justify-content-between header" :style="[isOpen ? 'min-width : 70vw' : 'min-width : 90vw']">
+  <div
+    class="d-flex justify-content-between header"
+  >
     <div>
-    <breadCrumbs></breadCrumbs>
+      <breadCrumbs></breadCrumbs>
+    </div>
+    <div>
+      <el-dropdown placement="bottom-start" trigger="click">
+        <el-button style="padding: 10px" type="info">
+          <i class="bx bx-user"></i>
+          <!-- <span style="margin-left: 5px">{{ user.email }}</span> -->
+        </el-button>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item
+              ><i class="bx bxs-edit"></i>Ma'lumotlarni
+              tahrirlash</el-dropdown-item
+            >
+            <el-dropdown-item @click="logOut"
+              ><i class="bx bx-exit"></i>Tizimdan chiqish</el-dropdown-item
+            >
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </div>
-  <div>
-    <el-dropdown placement="bottom-start" trigger="click">
-      <el-button style="padding: 10px" type="info">
-        <i class="bx bx-user"></i>
-        <!-- <span style="margin-left: 5px">{{ user.email }}</span> -->
-      </el-button>
-      <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item
-            ><i class="bx bxs-edit"></i>Ma'lumotlarni
-            tahrirlash</el-dropdown-item
-          >
-          <el-dropdown-item @click="logOut"
-            ><i class="bx bx-exit"></i>Tizimdan chiqish</el-dropdown-item
-          >
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
-  </div>
-</div>
 </template>
 <script>
-import { mapState } from 'pinia';
+import { mapState } from "pinia";
 import breadCrumbs from "./breadCrumbs.vue";
-import { userStore } from '../stores/management/user.store';
+import { userStore } from "../stores/management/user.store";
 export default {
   components: {
-    breadCrumbs
+    breadCrumbs,
   },
-  computed : {
-    ...mapState(userStore, ["currentUser"])
+  computed: {
+    ...mapState(userStore, ["currentUser"]),
   },
-  props : {
-    isOpen : {
-        type : Boolean,
-        default : false
-    }
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
   },
   methods: {
     logOut() {
